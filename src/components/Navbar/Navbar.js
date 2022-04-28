2a08d03b70c0e46480dfa5ca30394473c950ec46
Andres <afgo4ster@gmail.com>
2022-04-28 05:00:37 -0700

    add fonts

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,13 @@
 export default function Navbar (){
+
+    const marginLogo = {
+        margin: "20px"
+        
+    }
     
     return(
         <header className="header">
-            <div className="logo">logo</div>
+            <div className="logo"  style={marginLogo}>Supēsu</div>
             <ul className="links">
                 <li className="link">Inicio</li>
                 <li className="link">Productos</li>
