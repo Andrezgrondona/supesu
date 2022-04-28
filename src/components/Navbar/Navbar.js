f53b6cc903744764c16747270999572979a59cca
Andres <afgo4ster@gmail.com>
2022-04-28 04:40:49 -0700

    inicio git

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar (){
+    
+    return(
+        <header className="header">
+            <div className="logo">logo</div>
+            <ul className="links">
+                <li className="link">Inicio</li>
+                <li className="link">Productos</li>
+                <li className="link">Contacto</li>
+            </ul>
+        </header>
+    );
+}
