d0069b2bd97dbfde1b0ca131483edab099ac2352
Andres <afgo4ster@gmail.com>
2022-05-05 02:54:32 -0700

    add prop greeting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
+import Item from "./components/ItemListContainer/Item";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
      <Navbar category1={"Inicio"} category2={"Productos"} category3={"Contacto"}/>
+    
+     <Item greeting={"CREACIÓN DE ESPACIOS EXTRAORDINARIOS"} greeting2={"A LA MEDIDA DE TUS NECESIDADES"}/>
+     
     </div>
+ 
   );
 }
 
