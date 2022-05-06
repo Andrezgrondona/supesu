77d66b1310f2301d1a0f88b92ad3baeac04156c5
Andres <afgo4ster@gmail.com>
2022-05-06 04:25:17 -0700

    cambiando a componete funcional

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,24 +1,33 @@
-import React from "react"
-
-export default class ItemListContainer extends React.Component{
+ /* ----------------COMPONENTE DE CLASE---------------- */
+ 
+ /* import React from "react"
+    export default class ItemListContainer extends React.Component{
     render(){
         return(
             <div className="BannerHeader"> {this.props.greeting}</div>  
-        )
-        
+        )  
     }
-}
+}  */
+
+/* ----------------COMPONENTE DE CLASE---------------- */
 
 
+/* ----------------COMPONENTE DE FUNCIONAL---------------- */
 
-
-/* import React from "react"
-
+import React from "react"
 
 export default function ItemListContainer (){
-    const [] = React.useState(BannerHeader)
-        return(
-            <div className="BannerHeader"> {this.props.greeting}</div>  
-        )    
+  const [text, setText] = React.useState("CREACIÓN DE ESPACIOS EXTRAORDINARIOS")
+    return(
+        <div className="BannerHeader"> {text}</div>          
+    );
+}
 
-}  */
+/* ----------------COMPONENTE DE FUNCIONAL---------------- */
+ 
+ 
+ 
+ 
+ 
+
+
