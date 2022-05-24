b8012322ce1add4dac18437bde5f84709088b9b6
Andres <afgo4ster@gmail.com>
2022-05-23 17:06:00 -0700

    home style

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,13 +1,40 @@
 import React from 'react'
 import NavbarBoostrap from "../../components/NavbarBoostrap/NavbarBoostrap";
 import ItemListContainer from '../../components/ItemListContainer/ItemListContainer'
+import{Container, Col, Row} from 'react-bootstrap'
+
+import banner from '../../assets/images/baner4.jpeg'
 
 function Home() {
   return (
     <div className='AppRoute'>
       <NavbarBoostrap/>
-        
-        <ItemListContainer title="Lo mejor para tus espacios!"/> 
+
+      <Container className='fondoBaner'>
+        <div>
+        <img className='bannerUno' src={banner}/>
+        </div>
+      </Container>  
+
+      <Container>
+        <div className='babyBed'>
+          <div className='imgBaby'></div>
+          <div className='txtBabyBed'></div>
+
+        </div>
+      </Container>
+
+      <Container>
+      <div class="parent">
+        <div class="div1"> </div>
+        <div class="div2"> </div>
+        <div class="div3"> </div>
+        <div class="div4"> </div>
+      </div> 
+      </Container>
+
+
+      <ItemListContainer /> 
         
     </div>
   )
