007ac5d808b586ec89aaa5096965cf8447d8afe5
Andres <afgo4ster@gmail.com>
2022-05-24 12:06:42 -0700

    Home desing

---
+++ src/data/NewProducts.js
@@ -0,0 +1,14 @@
+export const NewProducts =[
+    
+
+    {
+        id:1,
+        title:"lampara",
+        profesion: Diseñador ,
+        price:400,
+        image:"../../img/mesa.jpeg",
+        detail:"La mesa comedor Florencia fue pensada para agregarle exclusividad a los espacios donde esté ubicada. Su diseño moderno y minimalista, fabricado con una estructura resistente, creará armonía al combinar piezas de diferentes tendencias",
+        category_id: 1,
+    },
+    
+]
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,36 +1,60 @@
 import React from 'react'
 import NavbarBoostrap from "../../components/NavbarBoostrap/NavbarBoostrap";
 import ItemListContainer from '../../components/ItemListContainer/ItemListContainer'
-import{Container, Col, Row} from 'react-bootstrap'
+import{Container, Col, Row, Alert, Button, Form} from 'react-bootstrap'
+
 
 import banner from '../../assets/images/baner4.jpeg'
+import baby from '../../assets/images/bebes.jpeg'
 
 function Home() {
   return (
     <div className='AppRoute'>
       <NavbarBoostrap/>
 
-      <Container className='fondoBaner'>
+      <Container>
         <div>
         <img className='bannerUno' src={banner}/>
         </div>
-      </Container>  
+      </Container> 
 
+     
       <Container>
-        <div className='babyBed'>
-          <div className='imgBaby'></div>
-          <div className='txtBabyBed'></div>
-
+        <div className='txt_succes'>
+          <p >Artículos destacados de la tienda</p>
+          <hr />
         </div>
-      </Container>
+      </Container>   
 
+        
       <Container>
       <div class="parent">
-        <div class="div1"> </div>
+        <div class="div1"></div>
         <div class="div2"> </div>
         <div class="div3"> </div>
         <div class="div4"> </div>
       </div> 
+      </Container>
+
+
+      <Container>
+        <div className='txt_succes'>
+        <Button variant="link">Ver productos!</Button>
+          <hr /> 
+        </div>
+      </Container> 
+
+      <Container>
+          <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Suscribete para recibir promociones</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              
+                <Button variant="primary" type="submit">
+                Enviar
+              </Button>
+              </Form.Group>
+      </Form>
       </Container>
 
 
