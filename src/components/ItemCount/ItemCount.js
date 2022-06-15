49d90fe42a2377868b97b3951e7cff932c4cf624
Andres <afgo4ster@gmail.com>
2022-06-15 13:03:05 -0700

    eliminando repo

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,53 +1,4 @@
-import React, { useState,useEffect } from 'react';
-import Card from 'react-bootstrap/Card'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
-
-
-
-export default function ItemCount({ stock, initial,  onAdd }){
-    const [count, setCount] = useState(+initial);
-
-    useEffect( () => {
-        setCount(count);
-    },[count])
-
-    const addCount = () => {
-        if (count < stock) {
-            setCount(count + 1);
-            onAdd(count);
-        }
-    };
-    const decreaseCount = () => {
-        if (count > 1) {
-            setCount(count - 1);
-            }
-    }
-
-    
-    return(
-        <div>                    
-            <Container>
-
-                                <Row>
-                                    <Col><button onClick={decreaseCount}> - </button></Col>
-                                    <Col>{count}</Col>
-                                    <Col><button onClick={addCount}> + </button></Col>
-                                </Row>                                
-        
-                </Container>
-        </div>
-        
-
-    )
-}
-
-
-
-
-/* import React, { useState } from "react";
+import React, { useState } from "react";
 
 const ItemCount =({stock}) => {
     const [counter, setCounter] = useState(1)
@@ -73,4 +24,4 @@ const ItemCount =({stock}) => {
 
 }
 
-export default ItemCount */
+export default ItemCount
