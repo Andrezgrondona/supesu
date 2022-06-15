898e6bc49a037f5119dbc7265bb07df80a7c22a7
Andres <afgo4ster@gmail.com>
2022-06-15 12:56:27 -0700

    check item detail

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,4 +1,53 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from 'react';
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+
+
+
+export default function ItemCount({ stock, initial,  onAdd }){
+    const [count, setCount] = useState(+initial);
+
+    useEffect( () => {
+        setCount(count);
+    },[count])
+
+    const addCount = () => {
+        if (count < stock) {
+            setCount(count + 1);
+            onAdd(count);
+        }
+    };
+    const decreaseCount = () => {
+        if (count > 1) {
+            setCount(count - 1);
+            }
+    }
+
+    
+    return(
+        <div>                    
+            <Container>
+
+                                <Row>
+                                    <Col><button onClick={decreaseCount}> - </button></Col>
+                                    <Col>{count}</Col>
+                                    <Col><button onClick={addCount}> + </button></Col>
+                                </Row>                                
+        
+                </Container>
+        </div>
+        
+
+    )
+}
+
+
+
+
+/* import React, { useState } from "react";
 
 const ItemCount =({stock}) => {
     const [counter, setCounter] = useState(1)
@@ -24,4 +73,4 @@ const ItemCount =({stock}) => {
 
 }
 
-export default ItemCount
+export default ItemCount */
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,16 +1,19 @@
-/* -------------------------------- FIREBASE -------------------------------- */
 
-import React from "react"
 import {Container, Row, Col, Card} from "react-bootstrap"
+import React from "react";
 import AddButton from "../AddButton/AddButton";
 import { CartContext } from "../../context/CartContext";
+
+
 export default function ItemDetail ({item}) {
-              //Definimos variables del context 
-  const { addToCart, isInCart } = React.useContext(CartContext);
-          //Definimos variables del context 
-  const [count, setCount] = React.useState(1)
- 
-  
+  const [count, setCount] = React.useState(0)
+  const {cart} = React.useContext(CartContext)
+  const {removeFromCart} = React.useContext(CartContext)
+  const {deleteAll} = React.useContext(CartContext)
+  const {addToCart} = React.useContext(CartContext)
+  const {countItems} = React.useContext(CartContext)
+  const {isInCart} = React.useContext(CartContext)
+
   return (
     <Card className="item-detail">
       <Card.Img variant="top" src={item.image} />
@@ -33,48 +36,6 @@ export default function ItemDetail ({item}) {
         )}
       </Card.Body>
     </Card>
+     
   );
 }
-
-/* -------------------------------- FIREBASE -------------------------------- */
-
-/* ------------------------------- ITEM NORMAL ------------------------------ */
-/* import React from "react"
-import {Container, Row, Col, Card} from "react-bootstrap"
-import AddButton from "../AddButton/AddButton";
-import { CartContext } from "../../context/CartContext";
-export default function ItemDetail ({item}) {
-              //Definimos variables del context 
-  const { addToCart, isInCart } = React.useContext(CartContext);
-          //Definimos variables del context 
-
-
-  
-  const [count, setCount] = React.useState(1)
- 
-  
-  return (
-    <Card className="item-detail">
-      <Card.Img variant="top" src={item.image} />
-      <Card.Body>
-        <Card.Title>{item.title}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <h3>$ {item.price}</h3>
-        {isInCart(item.id) ? (
-          <button>Ir al carrito</button>
-        ) : (
-          <AddButton
-            onSubmit={() => addToCart(item, count)}
-            count={count}
-            setCount={setCount}
-            stock={item.stock}
-          />
-        )}
-      </Card.Body>
-    </Card>
-  );
-} */
-/* ------------------------------- ITEM NORMAL ------------------------------ */
